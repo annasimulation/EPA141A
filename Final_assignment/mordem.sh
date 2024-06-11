4538278166b21d3b8bf5d29e1a77b4a6ef89c3ec
annasimulation <j.s.paardekooper@student.tudelft.nl>
2024-06-12 00:15:13 +0200

    delftblue

--- Final_assignment/mordem.sh
+++ Final_assignment/mordem.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --job-name="mordm_db"
 #SBATCH --partition=compute
-#SBATCH --account=research-education-TPM-MSc-EPA
+#SBATCH --account=innovation
 #SBATCH --time=03:00:00
 #SBATCH --ntasks=10
 #SBATCH --cpus-per-task=1
@@ -18,4 +18,4 @@ module load py-pip
 
 pip install --user --upgrade ema_workbench
 
-mpiexec -n 1 python3 Ass8.1(MORDM)(5runs).py
+mpiexec -n 1 python3 MORDM_test_db.py

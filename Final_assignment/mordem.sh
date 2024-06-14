71a9b1cfc128025b4d3d32735979d823fadc3e48
annasimulation <j.s.paardekooper@student.tudelft.nl>
2024-06-14 11:53:26 +0200

    change in script; & convergence metrics

--- Final_assignment/mordem.sh
+++ Final_assignment/mordem.sh
@@ -10,14 +10,21 @@
 #SBATCH --output=output_file.out
 #SBATCH --error=error_file.err
 
+# Load required modules
 module load 2023r1
 module load openmpi
 module load python
-module load py-numpy
-module load py-mpi4py
-module load py-scipy
 module load py-pip
 
-pip install --user --upgrade ema_workbench
+# Uninstall old numpy version
+pip uninstall -y numpy
 
-mpiexec -n 1 python3 MORDM_test_db.py
+# Install required packages
+pip install --user --upgrade numpy==1.26.4 ema_workbench
+
+# Export user base path for pip packages
+export PYTHONUSERBASE=$HOME/.local
+export PYTHONPATH=$HOME/.local/lib/python3.9/site-packages:$PYTHONPATH
+
+# Run the script using mpiexec
+mpiexec -n 10 python3 MORDM_test_db.py
